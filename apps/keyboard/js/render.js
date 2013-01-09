c46e5ece3b157d29efaa134be0b7b2b17e007a6e
pzhang <pzhang@mozilla.com>
2013-01-09 10:35:50 +0100

    Show customized keyboard instead

--- apps/keyboard/js/render.js
+++ apps/keyboard/js/render.js
@@ -175,7 +175,7 @@ const IMERender = (function() {
 
   // Highlight a key
   var highlightKey = function kr_updateKeyHighlight(key, alternativeKey) {
-    key.classList.add('highlighted');
+    key.classList.add('highlighted-show');
 
     if (alternativeKey) {
       var spanToReplace = key.querySelector('.visual-wrapper span');
@@ -185,7 +185,7 @@ const IMERender = (function() {
 
   // Unhighlight a key
   var unHighlightKey = function kr_unHighlightKey(key) {
-    key.classList.remove('highlighted');
+    key.classList.remove('highlighted-show');
   };
 
   // Show pending symbols with highlight (selection) if provided
@@ -493,7 +493,7 @@ const IMERender = (function() {
     }
 
 
-    var content = '<button class="keyboard-key ' + className + '"';
+    var content = '<button class="keyboard-key highlighted ' + className + '"';
     dataset.forEach(function(data) {
       content += ' data-' + data.key + '="' + data.value + '" ';
     });
@@ -547,7 +547,6 @@ const IMERender = (function() {
   var elem = null;
 
   function highlightNextKey() {
-    var start = new Date().getTime();
     window.clearTimeout(timeout);
 
     keys = document.getElementById('keyboard').querySelectorAll('.keyboard-key');
@@ -565,18 +564,13 @@ const IMERender = (function() {
     IMERender.highlightKey(elem);
     index = ++index % keys.length;
 
-    var end = new Date().getTime();
-    console.log('cost: ' + (end - start));
-
-    // timeout = window.setTimeout(highlightNextKey, 20);
+    timeout = window.setTimeout(highlightNextKey, 20);
   }
 
   function _startLoop(start) {
     if (start) {
-      console.log('start loop');
       highlightNextKey();
     } else {
-      console.log('end loop');
       window.clearTimeout(timeout);
     }
   }
@@ -602,6 +596,91 @@ const IMERender = (function() {
     'getHeight': getHeight,
     'getKeyArray': getKeyArray,
     'getKeyWidth': getKeyWidth,
-    'getKeyHeight': getKeyHeight
+    'getKeyHeight': getKeyHeight,
+    'startLoop': _startLoop
   };
 })();
+
+
+var Keyboard = (function() {
+  function createRow(keys) {
+    var rowElement = document.createElement('div');
+    rowElement.className = 'key-row';
+
+    keys.forEach(function(key) {
+      var keyElem = document.createElement('div');
+      keyElem.className = 'key-button';
+      keyElem.id = 'id-' + key.key;
+      keyElem.textContent = key.key;
+      keyElem.style.width = (key.width ? key.width : '32') + 'px';
+      keyElem.dataset.highlight = typeof key.highlight == 'undefined' ? true : key.highlight;
+      rowElement.appendChild(keyElem);
+    });
+
+    document.getElementById('keyboard-new').appendChild(rowElement);
+  }
+
+  var timeout = null;
+  function init() {
+    createRow([{key: 'q'}, {key: 'w'}, {key: 'e'}, {key: 'r'}, {key: 't'}, {key: 'y'}, {key: 'u'}, {key: 'i'}, {key: 'o'}, {key: 'p'}]);
+    createRow([{key: 'a'}, {key: 's'}, {key: 'd'}, {key: 'f'}, {key: 'g'}, {key: 'h'}, {key: 'j'}, {key: 'k'}, {key: 'l'}, {key: '\''}]);
+    createRow([{key: 'I', width: 48}, {key: 'z'}, {key: 'x'}, {key: 'c'}, {key: 'v'}, {key: 'b'}, {key: 'n'}, {key: 'm'}, {key: 'l', width: 48}]);
+    createRow([{key: '?123', width: 60, highlight: false}, {key: ','}, {key: 'x'}, {key: 'Space', width: 120, highlight: false}, {key: '.'}, {key: 'Enter', width: 43, highlight: false}]);
+
+    var keys = document.body.querySelectorAll('#keyboard-new .key-button');
+    var index = 10;
+
+    function highlightNextKey() {
+      var elem = keys[index];
+      highlightKey(elem);
+      index = ++index % keys.length;
+
+      timeout = window.setTimeout(highlightNextKey, 20);
+    }
+
+    highlightNextKey();
+
+    document.getElementById('id-Space').onclick = function onclick_space() {
+      if (timeout) {
+        window.clearTimeout(timeout);
+        timeout = null;
+      } else {
+        highlightNextKey();
+      }
+    };
+  }
+
+  var highlightedKeyElem = null;
+
+  function highlightKey(keyElem) {
+    if (highlightedKeyElem) {
+      highlightedKeyElem.classList.remove('highlighted');
+    }
+
+    highlightedKeyElem = keyElem;
+
+    var hlKey = document.getElementById('highlight');
+    keyElem.classList.add('highlighted');
+
+    if (keyElem.dataset.highlight == "true") {
+      hlKey.childNodes[0].textContent = keyElem.textContent;
+      hlKey.hidden = false;
+    } else {
+      hlKey.hidden = true;
+    }
+
+    hlKey.style.width = parseInt(keyElem.style.width) + 20 + 'px';
+    hlKey.style.left = '-10px';
+    keyElem.appendChild(hlKey);
+  }
+
+  window.addEventListener('load', function onload_wnd(event) {
+    window.removeEventListener('load', onload_wnd);
+    init();
+  });
+
+  return {
+    highlightKey: highlightKey
+  };
+})();
+
